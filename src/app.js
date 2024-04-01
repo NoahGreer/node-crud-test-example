390eb10b964bde6da9be5e3fec338120b7070847
Noah Greer <noah.greer@gmail.com>
2024-03-31 17:33:21 -0700

    Try clustering

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import os from "node:os";
+import cluster from "node:cluster";
+import process from "node:process";
 import NoteDao from "./dao/note-dao.js";
 import NoteService from "./service/note-service.js";
 import Server from "./api/server.js";
@@ -10,27 +13,47 @@ const databaseConnection = SqliteConnectionFactory.createConnection("./notes.db"
 const noteDao = new NoteDao(databaseConnection);
 const noteService = new NoteService(databaseConnection, noteDao);
 
-const app = new Server(noteService).buildApp();
+const numCpus = os.availableParallelism();
 
-const server = app.listen(port, () => {
-	logger.info(`Notes app listening at http://localhost:${port}/api`);
-});
+if (cluster.isPrimary) {
+	logger.info(`Primary ${process.pid} is running`);
 
-function gracefulShutdown() {
-	logger.info("closing HTTP server");
-	server.close(() => {
-		databaseConnection.close();
-		logger.info("HTTP server closed");
+	// Fork workers
+	for (let i = 0; i < numCpus; i++) {
+		cluster.fork();
+	}
+
+	cluster.on("exit", (worker, code, signal) => {
+		logger.info(`worker ${worker.process.pid} died`);
+	});
+
+	// Handle termination signal
+	process.on("SIGTERM", () => {
+		logger.info("Master received SIGTERM signal. Terminating workers...");
+
+		for (const worker of Object.values(cluster.workers)) {
+			worker.kill();
+		}
+
 		process.exit(0);
 	});
+} else {
+	// Worker process
+
+	const app = new Server(noteService).buildApp();
+
+	const server = app.listen(port, () => {
+		logger.info(`Notes app worker ${process.pid} listening at http://localhost:${port}/api`);
+	});
+
+	process.on("SIGTERM", () => {
+		logger.info("Worker received SIGTERM signal. Closing server...");
+		server.close(() => {
+			databaseConnection.close();
+			logger.info("Server closed");
+			process.exit(0);
+		});
+	});
+
+	logger.info(`Worker ${process.pid} started`);
 }
-
-process.on("SIGTERM", () => {
-	logger.info("SIGTERM signal received");
-	gracefulShutdown();
-});
-
-process.on("SIGINT", () => {
-	logger.info("SIGINT signal received");
-	gracefulShutdown();
-});
